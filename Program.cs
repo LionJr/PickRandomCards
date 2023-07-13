b3c83f866a2c7624aed781e1f0d7c41e9257d05e
LionJr <arslan.enigma.man@gmail.com>
2023-07-13 15:54:42 +0500

    Now we have complited our project that returns massive of random cards

--- Program.cs
+++ Program.cs
@@ -6,14 +6,47 @@ namespace PickRandomCards
   {
     static void Main(string[] args)
     {
-      
+      string[] cards = CardPicker.PickSomeCards(10);
+      foreach(string card in cards)
+      {
+        Console.WriteLine(card);
+      }
     }
   }
   class CardPicker
   {
+    static Random random = new Random();
     public static string[] PickSomeCards(int numberOfCards)
     {
-      
+      string[] pickedCards = new string[numberOfCards];
+      for(int i = 0; i < numberOfCards; i++)
+      {
+        pickedCards[i] = RandomValue() + " of " + RandomSuit();
+      }
+      return pickedCards;
+    }
+    private static string RandomSuit()
+    {
+      int value = random.Next(1,5); // 1-4 cenli
+      switch(value)
+      {
+        case 1: return "Spades";
+        case 2: return "Hearts";
+        case 3: return "Clubs";
+        default: return "Diamonds";
+      }
+    }
+    private static string RandomValue()
+    {
+      int value = random.Next(1,14);
+      switch(value)
+      {
+        case 1: return "Ace";
+        case 11: return "Jack";
+        case 12: return "Queen";
+        case 13: return "King";
+        default: return value.ToString();
+      }
     }
   }
 }
